fe19d2102d594ef0a42c7590286b89ccd1e84775
Bill Josh <58753666+BeyondBilly@users.noreply.github.com>
2020-05-25 18:18:06 -0500

    NOW WORKING - Fullscreen on webBrowser with Webview

--- app/src/main/java/com/example/webbrowserbedon/MainActivity.java
+++ app/src/main/java/com/example/webbrowserbedon/MainActivity.java
@@ -1,27 +1,30 @@
 package com.example.webbrowserbedon;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText txtURL;
     private Button bntGo;
     private WebView wbView;
     private String url;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +33,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtURL = findViewById(R.id.etxtURL);
         bntGo = findViewById(R.id.btnGo);
         wbView = findViewById(R.id.wbView);
+        progressBar = findViewById(R.id.progressbar);
 
         bntGo.setOnClickListener(this);
+
 
         //Visualizar el WebView
         wbView.setWebViewClient(new Browser_Home());
@@ -40,17 +45,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAppCacheEnabled(true);
+        loadWebSite();
 
         //Ajustar en pantalla completa
+
         wbView.getSettings().getLoadWithOverviewMode();
         wbView.getSettings().setUseWideViewPort(true);
-
-        loadWebSite();
 
     }
 
     private void loadWebSite() {
-        wbView.loadUrl("http://www.google.com"); //Cargar URL Predeterminada
+        ConnectivityManager cm = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            wbView.loadUrl("http://www.google.com"); //Cargar URL Predeterminada
+        } else{
+            wbView.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -69,6 +80,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onPageFinished(WebView view, String url) {
+            setTitle(view.getTitle());
+            progressBar.setVisibility(View.GONE);
             super.onPageFinished(view, url);
         }
     }
@@ -90,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     .getResources(), 2130837573);
         }
 
-        public void onHideCostumView(){
+        public void onHideCustomView(){
             ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
             this.mCustomView = null;
             getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
@@ -102,17 +115,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void onShowCustomView(View paramView, WebChromeClient.
                 CustomViewCallback paramCustomViewCallback){
             if (this.mCustomView != null){
-                onHideCostumView();
+                onHideCustomView();
                 return;
             }
             this.mCustomView = paramView;
             this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
             this.mOriginalOrientation = getRequestedOrientation();
             this.mCustomViewCallback = paramCustomViewCallback;
-            ((FrameLayout)getWindow().getDecorView()).
-                    addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
-            getWindow().getDecorView().
-                    setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.
+                    LayoutParams(-1, -1));
+            getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         }
     }
 }
